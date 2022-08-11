17554200b8dfed627c998e410eb487d9c0576756
Alexander Pelta <alexpxrad@gmail.com>
2022-08-11 13:43:40 -0400

    added google login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,58 +1,105 @@
-import { useState } from 'react'
-import { initializeApp } from 'firebase/app'
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
-
-
+import { useState } from "react";
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBJxfQ8G0guqVNjAgURKddTUEYvP__GPPg",
-    authDomain: "first-login-abp.firebaseapp.com",
-    projectId: "first-login-abp",
-    storageBucket: "first-login-abp.appspot.com",
-    messagingSenderId: "797689052122",
-    appId: "1:797689052122:web:e947f3ab3a7e0520091ee3"
-  };
-
+  apiKey: "AIzaSyAHiX25Jq-lURNmjlLlYWpUHM2GSnCEzxM",
+  authDomain: "first-login-9893a.firebaseapp.com",
+  projectId: "first-login-9893a",
+  storageBucket: "first-login-9893a.appspot.com",
+  messagingSenderId: "821654267894",
+  appId: "1:821654267894:web:523e9eeffafe89943409d7",
+};
 
 function Login({ setIsLoggedIn }) {
-    const [email, setEmail] =useState('')
-    const [password, setPasssword] =useState('')
-    const handleSignUp = async () => {
+  const [email, setEmail] = useState("");
+  const [password, setPasssword] = useState("");
 
-        //connect to firebase proj
-        const app = initializeApp(firebaseConfig)
-        //connnect to Auth
+  const connectAuth = async () => {
+    const app = initializeApp(firebaseConfig);
+    return getAuth(app);
+  };
 
-        const auth = getAuth(app);
-        //send email and password to firebase auth
-        const user = await createUserWithEmailAndPassword(auth, email, password)
-        .catch(err => alert(err.message))
-        //if all ok..
-        if(user) {
-            console.log(user)
-            setIsLoggedIn(true)
-
-        }
-        //if error 
-        //popup error
+  const handleLogin = async () => {
+    const auth = await connectAuth();
+    const user = signInWithEmailAndPassword(auth, email, password).catch(
+      (err) => alert(err.message)
+    );
+    if (user) {
+      console.log(user.user);
+      setIsLoggedIn(true);
     }
-        return (
-                <form onSubmit={(e) => e.preventDefault}>
-                    <label htmlFor="email">
-                        Email: 
-                        <input 
-                        value={email} onChange={e => setEmail(e.target.value)}
-                        name="email" type="email" placeholder="you@there.com"/> 
-                    </label><br/>
-                    <label for="password">
-                        Password: 
-                        <input 
-                        value={password} onChange={e => setPasssword(e.target.value)}
-                        name="password" type="password"/> 
-                    </label><br/>
-                    <button onClick={handleSignUp}>Sign up</button>
-                </form>
-                 )
-                }
+  };
 
-export default Login; 
+  const handleGoogleLogin = async () => {
+    const auth = await connectAuth()
+    const provider = new GoogleAuthProvider()
+    const user = await signInWithPopup()(auth, provider)
+        .catch(err => alert(err.message))
+    if(user) {
+        console.log(user.user)
+        setIsLoggedIn(true)
+
+    }
+
+
+  }
+
+
+
+  const handleSignUp = async () => {
+    //connect to firebase proj
+    //connnect to Auth
+    const auth = await connectAuth();
+    //send email and password to firebase auth
+    const user = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    ).catch((err) => alert(err.message));
+    //if all ok..
+    if (user) {
+      console.log(user);
+      setIsLoggedIn(true);
+    }
+    //if error
+    //popup error
+  };
+  return (
+    <form onSubmit={(e) => e.preventDefault}>
+      <label htmlFor="email">
+        Email:
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name="email"
+          type="email"
+          placeholder="you@there.com"
+        />
+      </label>
+      <br />
+      <label for="password">
+        Password:
+        <input
+          value={password}
+          onChange={(e) => setPasssword(e.target.value)}
+          name="password"
+          type="password"
+        />
+      </label>
+      <br />
+      <button onClick={handleLogin}>Login</button>&nbsp;
+      <button onClick={handleSignUp}>Sign up</button>
+      <br/>
+      <button onClick={handleGoogleLogin}>Login with Google</button>
+    </form>
+  );
+}
+
+export default Login;
